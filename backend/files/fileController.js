9a33fef2a8113892ac28a1e2d4d178a505d8f18a
applyinnovations <alexander@applyinnovations.com.au>
2024-09-01 21:00:14 +1000

    removed header

--- backend/files/fileController.js
+++ backend/files/fileController.js
@@ -2,7 +2,6 @@ const crypto = require('crypto');
 
 module.exports = {
   encryptFile: (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
     // Encrypt the file and save it
     const aesKey = crypto.randomBytes(32);
     const aesIv = crypto.randomBytes(12);
