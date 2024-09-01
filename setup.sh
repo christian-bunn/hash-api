39b59d2e8e5fe648dcaa04fb1e8f7eeb0ee15463
Christian Bunn <ctbunn333@gmail.com>
2024-09-01 18:05:29 +1000

    mkdir command

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 
 sudo snap install docker
 sudo chown $USER /var/run/docker.sock
+mkdir ./backend/keys
 openssl genrsa -out ./backend/keys/private.pem 4096
 openssl rsa -in ./backend/keys/private.pem -pubout -out ./backend/keys/public.pem
