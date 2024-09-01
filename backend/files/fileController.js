ec25a356977008acfbb99e56c4dfbc4246565140
applyinnovations <alexander@applyinnovations.com.au>
2024-09-01 21:16:06 +1000

    changed alg

--- backend/files/fileController.js
+++ backend/files/fileController.js
@@ -2,29 +2,24 @@ const crypto = require('crypto');
 
 module.exports = {
   encryptFile: (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
+    // res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
     // Encrypt the file and save it
     const aesKey = crypto.randomBytes(32);
     const aesIv = crypto.randomBytes(12);
-    const cipher = crypto.createCipheriv('aes-256-ccm', aesKey, aesIv, {
-      authTagLength: 16,
+    const cipher = crypto.createCipheriv('aes-256-cbc', aesKey, aesIv);
+
+    req.on('data', (chunk) => {
+      try {
+        res.write(cipher.update(chunk));
+      } catch (err) {
+        res.statusCode = 500;
+        res.end(`Error during encryption: ${err.message}`);
+      }
     });
-
-    req.pipe(cipher).pipe(res);
-
-    //    req.on('data', (chunk) => {
-    //      try {
-    //        res.write(cipher.update(chunk));
-    //      } catch (err) {
-    //        res.statusCode = 500;
-    //        res.end(`Error during encryption: ${err.message}`);
-    //      }
-    //    });
 
     req.on('end', () => {
       try {
         res.write(cipher.final());
-        res.write(cipher.getAuthTag());
         res.end();
       } catch (err) {
         res.statusCode = 500;
