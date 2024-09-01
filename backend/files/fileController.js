f311074c42dfc72f84bfc03d655348ae0dcd9b46
applyinnovations <alexander@applyinnovations.com.au>
2024-09-01 20:40:49 +1000

    updated params for ccm

--- backend/files/fileController.js
+++ backend/files/fileController.js
@@ -5,8 +5,10 @@ module.exports = {
     res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
     // Encrypt the file and save it
     const aesKey = crypto.randomBytes(32);
-    const aesIv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv('aes-256-ccm', aesKey, aesIv);
+    const aesIv = crypto.randomBytes(12);
+    const cipher = crypto.createCipheriv('aes-256-ccm', aesKey, aesIv, {
+      authTagLength: 16,
+    });
 
     req.on('data', (chunk) => {
       try {
