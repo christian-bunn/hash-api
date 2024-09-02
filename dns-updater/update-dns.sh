8dcf16db558a193d9d6ab7a158e5c86774d9fb64
Christian Bunn <ctbunn333@gmail.com>
2024-09-02 10:18:38 +1000

    updated script to run every hour

--- dns-updater/update-dns.sh
+++ dns-updater/update-dns.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 IP=$(curl -s ipinfo.io/ip)
+echo "updating ip to $IP"
 
 curl -s "https://dynamicdns.park-your-domain.com/update?host=@&domain=ilovequt.lol&password=b3d9fe22e68f46488e2311901031ab78&ip=$IP"
 curl -s "https://dynamicdns.park-your-domain.com/update?host=*&domain=ilovequt.lol&password=b3d9fe22e68f46488e2311901031ab78&ip=$IP"
+
+echo "sleeping for 1 hour..."
+sleep 3600
