846c523202071dce8e013d944b8c7748824392bd
Christian Bunn <ctbunn333@gmail.com>
2024-09-02 10:14:02 +1000

    removed progress bar from curl

--- dns-updater/update-dns.sh
+++ dns-updater/update-dns.sh
@@ -2,5 +2,5 @@
 
 IP=$(curl -s ipinfo.io/ip)
 
-curl "https://dynamicdns.park-your-domain.com/update?host=@&domain=ilovequt.lol&password=b3d9fe22e68f46488e2311901031ab78&ip=$IP"
-curl "https://dynamicdns.park-your-domain.com/update?host=*&domain=ilovequt.lol&password=b3d9fe22e68f46488e2311901031ab78&ip=$IP"
+curl -s "https://dynamicdns.park-your-domain.com/update?host=@&domain=ilovequt.lol&password=b3d9fe22e68f46488e2311901031ab78&ip=$IP"
+curl -s "https://dynamicdns.park-your-domain.com/update?host=*&domain=ilovequt.lol&password=b3d9fe22e68f46488e2311901031ab78&ip=$IP"
